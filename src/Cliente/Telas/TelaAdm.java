e1df0854c2121364319d31bce1e851d5db48756f
ismae <ismaeltoledodemedeiros@gmail.com>
2022-07-02 16:45:19 -0300

    configuração da tela de visualização de salas

--- src/Cliente/Telas/TelaAdm.java
+++ src/Cliente/Telas/TelaAdm.java
@@ -5,8 +5,11 @@
  */
 package Cliente.Telas;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import Cliente.Sala;
 import Cliente.Conexao;
 
 /**
@@ -26,32 +29,32 @@ public class TelaAdm extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        listaSalas = new javax.swing.JList<>();
+        criar = new javax.swing.JButton();
+        entrar = new javax.swing.JButton();
+        idSala = new javax.swing.JTextField();
         label1 = new java.awt.Label();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        updateSalas();
 
+        jScrollPane1.setViewportView(listaSalas);
 
-        jScrollPane1.setViewportView(jList1);
-
-        jButton1.setText("Criar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        criar.setText("Criar");
+        criar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Entrar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        entrar.setText("Entrar");
+        entrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                onEntrar(evt);
             }
         });
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        idSala.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField1ActionPerformed(evt);
             }
@@ -76,16 +79,16 @@ public class TelaAdm extends javax.swing.JFrame {
                                                         javax.swing.GroupLayout.DEFAULT_SIZE,
                                                         javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addGap(18, 18, 18)
-                                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 100,
+                                                .addComponent(idSala, javax.swing.GroupLayout.PREFERRED_SIZE, 100,
                                                         javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addGap(81, 81, 81))
                                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout
                                                 .createSequentialGroup()
                                                 .addGroup(layout
                                                         .createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                        .addComponent(criar, javax.swing.GroupLayout.PREFERRED_SIZE,
                                                                 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                        .addComponent(entrar, javax.swing.GroupLayout.PREFERRED_SIZE,
                                                                 126, javax.swing.GroupLayout.PREFERRED_SIZE))
                                                 .addGap(65, 65, 65)))));
         layout.setVerticalGroup(
@@ -93,17 +96,17 @@ public class TelaAdm extends javax.swing.JFrame {
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(150, 150, 150)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                        .addComponent(idSala, javax.swing.GroupLayout.PREFERRED_SIZE,
                                                 javax.swing.GroupLayout.DEFAULT_SIZE,
                                                 javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE,
                                                 javax.swing.GroupLayout.DEFAULT_SIZE,
                                                 javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(18, 18, 18)
-                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 42,
+                                .addComponent(entrar, javax.swing.GroupLayout.PREFERRED_SIZE, 42,
                                         javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 42,
+                                .addComponent(criar, javax.swing.GroupLayout.PREFERRED_SIZE, 42,
                                         javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -112,20 +115,45 @@ public class TelaAdm extends javax.swing.JFrame {
                                         javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(37, 37, 37)));
 
-        jTextField1.getAccessibleContext().setAccessibleName("id_sala");
-        jTextField1.getAccessibleContext().setAccessibleDescription("");
+        idSala.getAccessibleContext().setAccessibleName("id_sala");
+        idSala.getAccessibleContext().setAccessibleDescription("");
 
         pack();
+        evento();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void evento(){
+        listaSalas.addListSelectionListener(e->{
+                Object aux = ((JList) e.getSource()).getSelectedValue();
+                if(aux != null){
+                        idSala.setText(((Sala) aux).id+"");
+                }
+        });
+    }
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }// GEN-LAST:event_jTextField1ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton2ActionPerformed
-        this.dispose();
-        new TelaJogoMulti().setVisible(true);
+    private void onEntrar(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton2ActionPerformed
+        // this.dispose();
+        // new TelaJogoMulti().setVisible(true);
+        String id = idSala.getText();
+        if(id.isEmpty()){
+                return;
+        }
 
+        try {
+                String dados = Conexao.get().conexao("entrar_sala;-;{id:"+id+"}");
+                if(dados.equals("ok")){
+                        new TelaJogoMulti();
+                }
+                
+        } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+        
     }// GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton1ActionPerformed
@@ -134,30 +162,34 @@ public class TelaAdm extends javax.swing.JFrame {
     }// GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JList<String> jList1;
+    private javax.swing.JButton criar;
+    private javax.swing.JButton entrar;
+    private javax.swing.JList<Sala> listaSalas;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField idSala;
     private java.awt.Label label1;
     // End of variables declaration//GEN-END:variables
 
     private void updateSalas(){
+            ArrayList<Sala> salas = new ArrayList<Sala>();
         try {
-              String dados = Conexao.get().conexao("lista_sala:");
+              String dados = Conexao.get().conexao("lista_sala;-;");
+
+              for(String s : dados.split(",")){
+                salas.add(new Sala(s));
+              }
         } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
         }
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-                String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+        listaSalas.setModel(new javax.swing.AbstractListModel<Sala>() {
 
                 public int getSize() {
-                        return strings.length;
+                        return salas.size();
                 }
 
-                public String getElementAt(int i) {
-                        return strings[i];
+                public Sala getElementAt(int i) {
+                        return salas.get(i);
                 }
         });
     }
